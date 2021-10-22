5460b9b27e3e5f182ad1a96c5780943f19781bce
BeccaPirie <43371064+BeccaPirie@users.noreply.github.com>
2021-10-22 15:00:25 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helment = require('helmet');
+const morgan = require('morgan');
+const indexRoute = require("./routes/index")
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, () => {
+    console.log("Connected to MONGODB")
+});
+
+//middleware
+app.use(express.json());
+app.use(helment());
+app.use(morgan("common"));
+
+app.use('/', indexRoute);
+app.use("/users", userRoute);
+app.use("/auth", authRoute);
+
+app.listen(5000, () => {
+    console.log("backend server is running")
+})
+
