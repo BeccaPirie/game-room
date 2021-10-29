702c61bfe61c389421db940a882ffda13c9b89ef
BeccaPirie <43371064+BeccaPirie@users.noreply.github.com>
2021-10-29 17:02:14 +0100

    fix modal

--- client/src/components/selectedGame/SelectedGame.jsx
+++ client/src/components/selectedGame/SelectedGame.jsx
@@ -3,16 +3,16 @@ import StarBorderIcon from '@mui/icons-material/StarBorder';
 import StarIcon from '@mui/icons-material/Star';
 import CloseIcon from '@mui/icons-material/Close';
 
-export default function SelectedGame({modalHandler}) {
+export default function SelectedGame() {
     return (
         <div className="selectedGameContainer">
-            <div className="button cancel" onClick={modalHandler}><CloseIcon /></div>
-            <div className="button addFavourite"><StarBorderIcon /></div>
+            {/* <div className="button cancel" onClick={modalHandler}><CloseIcon /></div> */}
             <img src='../../assets/profile-pic.jpg' alt="" className="thumbnail" />
             <div className="gameBtns">
                 <button className="button leaderboardBtn">Leaderboard</button>
+                <button className="button addFavourite">Add to favourites</button>
                 <button className="button playBtn">Play</button> 
-            </div>
+            </div>            
         </div>
     )
 }
