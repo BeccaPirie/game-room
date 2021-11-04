60e7dabff204ac9db192b734c735295c9e2a802c
BeccaPirie <43371064+BeccaPirie@users.noreply.github.com>
2021-11-04 15:24:19 +0000

    register system

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,19 +1,75 @@
 import './register.scss'
+import { useRef, useContext } from 'react'
+import { AuthContext } from '../../context/AuthContext'
+import { CircularProgress } from '@mui/material'
+import axios from 'axios'
+import { useHistory } from 'react-router'
 
 export default function Register() {
+    const username = useRef()
+    const email = useRef()
+    const password = useRef()
+    const confirmPassword = useRef()
+    const history = useHistory()
+
+    const handleClick = async (e) => {
+        e.preventDefault()
+
+        if(confirmPassword.current.value !== password.current.value) {
+            confirmPassword.current.setCustomValidity("Passwords don't match")
+        } else {
+            const user = {
+                username: username.current.value,
+                email: email.current.value,
+                password: password.current.value,
+                confirmPassword: confirmPassword.current.value
+            }
+            try {
+                await axios.post("/auth/register", user)
+                history.push('/login')
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        
+    }
+
     return (
         <div className="login">
-
-            <div className="loginBox">
+            <form className="loginBox" onSubmit={handleClick}>
                 <h3 className="loginTitle">Game Room</h3>
-                <input placeholder="Username" className="loginInput" />
-                <input placeholder="Email" className="loginInput" />
-                <input placeholder="Password" className="loginInput" />
-                <input placeholder="Password Again" className="loginInput" />
+                <input
+                    required
+                    placeholder="Username"
+                    className="loginInput"
+                    ref={username}
+                />
+                <input
+                    type="email"
+                    required
+                    re
+                    placeholder="Email"
+                    className="loginInput"
+                    ref={email}
+                />
+                <input
+                    type="password"
+                    required
+                    minLength="6"
+                    placeholder="Password"
+                    className="loginInput"
+                    ref={password}
+                />
+                <input
+                    type="password"
+                    required
+                    placeholder="Confirm Password"
+                    className="loginInput"
+                    ref={confirmPassword}
+                />
                 <button className="loginButton">Sign Up</button>
                 <button className="loginRegisterButton">Log into Account</button>
-            </div>
-
+            </form>
         </div>
     )
 }
